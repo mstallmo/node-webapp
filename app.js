65d04bb68ad9cfbd2db0c08998d7b31633b47cb1
Mason Stallmo <masonstallmo@gmail.com>
2017-08-26 16:45:40 -0500

    Setup gulp

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 
-let port = 1337;
+let port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    res.send('Hello World');
 });
 
 app.get('/books', function (req, res) {
-    res.send("Hello Books");
+    res.send('Hello Books');
 });
-
 
 app.listen(port, function(err) {
     console.log('Running server on port: ' + port);
